9a7df35245a36cbfda460f37b9a0d27b6901a742
木棉流年 <oncwnuJ3tM0JDIzIoN6gbKFltye0@git.weixin.qq.com>
2024-05-13 00:29:24 +0800

    first commit

---
+++ src/main/java/com/hh/mayipapa/MayipapaApplication.java
@@ -0,0 +1,13 @@
+package com.hh.mayipapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayipapaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MayipapaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hh/mayipapa/MayipapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.mayipapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayipapaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
